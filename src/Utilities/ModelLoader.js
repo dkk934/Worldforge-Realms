5e17a3c103a862fde8230628c053e63b46747978
DK <128988989+dkk934@users.noreply.github.com>
2025-09-04 20:50:43 +0530

    Update ModelLoader.js

--- src/Utilities/ModelLoader.js
+++ src/Utilities/ModelLoader.js
@@ -7,7 +7,7 @@ export class ModelLoader {
   };
 
   loadModels(onLoad) {
-    this.loader.load("/model/pickaxe.glb", (model) => {
+    this.loader.load("./model/pickaxe.glb", (model) => {
       const mesh = model.scene;
       this.models.pickaxe = mesh;
       onLoad(this.models);
